87378738ba05263f87140d1c4d3e50e64987f305
Harry Kightlinger <kightlingerh@gmail.com>
2020-02-06 12:59:22 -0800

    Formatting

--- src/components/form/radio/BRadio.ts
+++ src/components/form/radio/BRadio.ts
@@ -2,9 +2,9 @@ import './radio.sass';
 import { getSelectionControl } from '../shared/getSelectionControl';
 
 export default getSelectionControl('radio', 'radio', 'BRadio', 'b-radio radio').extend({
-	computed: {
-		disableOnChange(): boolean {
-			return this.isActive;
-		}
-	}
+  computed: {
+    disableOnChange(): boolean {
+      return this.isActive;
+    }
+  }
 });
