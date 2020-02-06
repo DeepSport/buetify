68965ff0c1acd815cb72b7cb74e790e36c65e399
Harry Kightlinger <kightlingerh@gmail.com>
2020-02-06 12:47:54 -0800

    Fixed b-radio onChange event handler

--- src/components/form/radio/BRadio.ts
+++ src/components/form/radio/BRadio.ts
@@ -1,4 +1,10 @@
 import './radio.sass';
 import { getSelectionControl } from '../shared/getSelectionControl';
 
-export default getSelectionControl('radio', 'radio', 'BRadio', 'b-radio radio');
+export default getSelectionControl('radio', 'radio', 'BRadio', 'b-radio radio').extend({
+	computed: {
+		disableOnChange(): boolean {
+			return this.isActive;
+		}
+	}
+});
