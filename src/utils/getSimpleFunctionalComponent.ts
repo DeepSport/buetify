c13dddadc14ed6651205e4197ef34645f96f01cd
Harry Kightlinger <kightlingerh@gmail.com>
2020-05-27 19:25:08 -0700

    Finished BImage and removed bordered box

--- src/utils/getSimpleFunctionalComponent.ts
+++ src/utils/getSimpleFunctionalComponent.ts
@@ -2,9 +2,9 @@ import { h, SetupContext } from 'vue';
 import { Classes, mergeClasses } from './mergeClasses';
 
 export function getSimpleFunctionalComponent(cls: string, el: string = 'div') {
-  return function(_:any, { attrs, slots }: SetupContext) {
+  return function(props: { tag?: string }, { attrs, slots }: SetupContext) {
     attrs.class = mergeClasses(attrs.class as Classes, cls)
-    return h(el, attrs, slots.default ? slots.default() : undefined)
+    return h(props.tag ?? el, attrs, slots.default ? slots.default() : undefined)
   }
 }
 
