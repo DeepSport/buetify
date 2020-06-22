d395260fb8d321a1d7b3c9d7eb4b3f889dcb7e40
Harry Kightlinger <kightlingerh@gmail.com>
2020-06-22 16:14:28 -0700

    Bump vue

--- src/utils/getSimpleFunctionalComponent.ts
+++ src/utils/getSimpleFunctionalComponent.ts
@@ -1,8 +1,8 @@
-import { h, SetupContext } from 'vue';
+import { h, FunctionalComponent } from 'vue';
 import { Classes, mergeClasses } from './mergeClasses';
 
-export function getSimpleFunctionalComponent(cls: string, el: string = 'div') {
-  return function(props: { tag?: string }, { attrs, slots }: SetupContext) {
+export function getSimpleFunctionalComponent(cls: string, el: string = 'div'): FunctionalComponent<{ tag?: string }> {
+  return function(props, { attrs, slots }) {
     attrs.class = mergeClasses(attrs.class as Classes, cls)
     return h(props.tag ?? el, attrs, slots.default ? slots.default() : undefined)
   }
