08ae49675d931b1831ff9efe159da53737c16124
Harry Kightlinger <kightlingerh@gmail.com>
2020-06-25 13:59:50 -0700

    Added toast and snackbar composables

--- src/components/notices/toast/BToast.ts
+++ src/components/notices/toast/BToast.ts
@@ -1,36 +1,12 @@
-import { constant, FunctionN } from 'fp-ts/lib/function';
-import { IO } from 'fp-ts/lib/IO';
-import {
-  RenderNoticeOptions,
-  useNoticeController,
-  UseNoticePropsDefinition
-} from '../../../composables/noticeController';
-import { VNode, defineComponent, shallowRef, h } from 'vue';
-import { alwaysEmptyArray } from '../../../utils/helpers';
+import { UseNoticePropsDefinition } from '../../../composables/noticeController';
+import { defineComponent } from 'vue';
+import { useToast } from '../../../composables/toast';
 
 export default defineComponent({
   name: 'b-toast',
   props: UseNoticePropsDefinition,
   setup(props, { slots }) {
-    const renderNotification = shallowRef(constant(alwaysEmptyArray) as FunctionN<[RenderNoticeOptions], IO<VNode[]>>);
-    const noticeController = useNoticeController(props, renderNotification);
-    renderNotification.value = options => () => {
-      return [
-        h(
-          'div',
-          {
-            class: ['toast', options.variant ?? props.variant, options.position ?? props.position],
-            role: 'alert'
-          },
-          options.message ?? (slots.message && slots.message()) ?? props.message
-        )
-      ];
-    };
-    return () =>
-      slots.default &&
-      slots.default({
-        open: noticeController.open,
-        clsoe: noticeController.close
-      });
+    const controller = useToast(props, slots);
+    return () => slots.default && slots.default(controller);
   }
 });
---
+++ src/composables/toast/useToast.ts
@@ -0,0 +1,23 @@
+import { constant, FunctionN } from 'fp-ts/lib/function';
+import { IO } from 'fp-ts/lib/IO';
+import { h, shallowRef, VNode, Slots } from 'vue';
+import { alwaysEmptyArray } from '../../utils/helpers';
+import { RenderNoticeOptions, useNoticeController, UseNoticeProps } from '../noticeController';
+
+export function useToast(props: UseNoticeProps, slots: Slots) {
+  const renderNotification = shallowRef(constant(alwaysEmptyArray) as FunctionN<[RenderNoticeOptions], IO<VNode[]>>);
+  const noticeController = useNoticeController(props, renderNotification);
+  renderNotification.value = options => () => {
+    return [
+      h(
+        'div',
+        {
+          class: ['toast', options.variant ?? props.variant, options.position ?? props.position],
+          role: 'alert'
+        },
+        options.message ?? (slots.message && slots.message()) ?? props.message
+      )
+    ];
+  };
+  return noticeController;
+}
