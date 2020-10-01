73da85ba44793466f16314316c87503edf018fbf
Harry Kightlinger <kightlingerh@gmail.com>
2020-09-30 21:37:37 -0700

    removed js files

--- packages/docs/src/components/icons/GithubIcon.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var faGithub_1 = require("@fortawesome/free-brands-svg-icons/faGithub");
-var utils_1 = require("buetify/lib/components/icons/utils");
-exports["default"] = utils_1.useIconComponent('Github', faGithub_1.faGithub);
--- packages/docs/src/views/documentation/menu.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.menu = void 0;
-var components = require("./components");
-exports.menu = [components.menu];
