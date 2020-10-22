f22f96178db3bdedc1bd97d5a9ad56b4912dd3ff
Harry Kightlinger <kightlingerh@gmail.com>
2020-10-22 16:37:08 -0700

    Added netifly config

--- packages/docs/vue.config.js
+++ packages/docs/vue.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const isProduction = process.env.NODE_ENV === 'production'
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
 	parallel: false,
@@ -19,12 +19,19 @@ module.exports = {
 		resolve: {
 			alias: {
 				'@': path.resolve(__dirname, './src'),
-				'vue$': isProduction ? path.resolve(__dirname, '../../node_modules/vue/dist/vue.runtime.esm-browser.js') : path.resolve(__dirname, '../../node_modules/vue'),
+				vue$: isProduction
+					? path.resolve(__dirname, '../../node_modules/vue/dist/vue.runtime.esm-browser.js')
+					: path.resolve(__dirname, '../../node_modules/vue'),
 				'fp-ts/lib': 'fp-ts/es6/',
 				'io-ts/lib': 'io-ts/es6/',
 				'buetify/src': path.resolve(__dirname, '../buetify/src/'),
 				'buetify/lib': path.resolve(__dirname, '../buetify/src/')
 			}
+		}
+	},
+	pwa: {
+		workboxOptions: {
+			exclude: [/_redirects/]
 		}
 	}
 };
