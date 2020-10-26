b6ced6ad29e9f134ad4af786a7ad43784286543d
Harry Kightlinger <kightlingerh@gmail.com>
2020-10-26 11:40:54 -0700

    Removed js files

--- packages/docs/src/views/documentation/components/form/input/menu.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.menu = void 0;
-var BuetifyMenu_1 = require("../../../shared/BuetifyMenu");
-var meta_1 = require("./meta");
-exports.menu = BuetifyMenu_1.link(meta_1.meta.title, meta_1.meta.fullPath);
-//# sourceMappingURL=menu.js.map
